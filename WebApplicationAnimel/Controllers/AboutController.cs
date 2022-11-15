d48a00f0491ba8fe8205ed59e57565d036430d63
eitanch9 <106733760+eitanch9@users.noreply.github.com>
2022-11-16 00:09:16 +0200

    add page About

---
+++ WebApplicationAnimel/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationAnimel.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
